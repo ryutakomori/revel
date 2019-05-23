549c7dd5a39ba19da8abfe3d7de496f1c43176b6
ryuta <komori@c-2-d.com>
2019-05-23 14:07:47 +0900

    キャッシュ削除

--- api/app/util/msg/msg.go
+++
@@ -1,9 +0,0 @@
-package msg
-
-// func Tpl(msg string) string {
-// 	return `{"errors": {"Password" : {"message": "` + msg + `"}}}`
-// }
-
-func Tpl(column string, msg string) string {
-	return `{"errors": {"` + column + `" : {"message": "` + msg + `"}}}`
-}
--- docker/mysql/sql/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
- );
